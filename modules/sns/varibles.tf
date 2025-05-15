290e4aeddfd00654593414f2dd8006a172f36b34
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-15 11:58:55 +0530

    Create varibles.tf

---
+++ modules/sns/varibles.tf
@@ -0,0 +1,10 @@
+variable "topic_name" {
+  type        = string
+  description = "SNS Topic Name"
+}
+
+variable "subscription_email" {
+  type        = string
+  description = "Email address to subscribe to the SNS topic (optional)"
+  default     = ""
+}
