b7717414bc0adaa00b984adde1abac71f0416e3a
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-15 12:02:01 +0530

    Create main.tf

---
+++ modules/iam/main.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "lambda_execution_role" {
+  name = var.lambda_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_full_access" {
+  name        = "${var.lambda_role_name}-policy"
+  description = "Policy for Lambda with S3, DynamoDB, SNS and CloudWatch access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:*",
+          "dynamodb:*",
+          "sns:*",
+          "logs:*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_full_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
