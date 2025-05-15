9b0bb306627e6797858e4c9c358f12c05780e59c
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-15 13:13:34 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,27 +1,27 @@
 resource "aws_lambda_function" "upload_patient_document" {
-  filename         = "${path.module}/upload_patient_document.zip"
+  filename         = "${var.lambda_source_dir}/upload_patient_document.zip"
   function_name    = "UploadPatientDocumentFunction"
   role             = var.lambda_role_arn
   handler          = "UploadPatientDocumentFunction.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("${path.module}/upload_patient_document.zip")
+  source_code_hash = filebase64sha256("${var.lambda_source_dir}/upload_patient_document.zip")
 
   environment {
     variables = {
-      BUCKET_NAME   = var.s3_bucket
-      TABLE_NAME    = var.dynamodb_table
-      TOPIC_ARN     = var.sns_topic_arn
+      BUCKET_NAME = var.s3_bucket
+      TABLE_NAME  = var.dynamodb_table
+      TOPIC_ARN   = var.sns_topic_arn
     }
   }
 }
 
 resource "aws_lambda_function" "update_prescription" {
-  filename         = "${path.module}/update_prescription.zip"
+  filename         = "${var.lambda_source_dir}/update_prescription.zip"
   function_name    = "UpdatePrescriptionFunction"
   role             = var.lambda_role_arn
   handler          = "UpdatePrescriptionFunction.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("${path.module}/update_prescription.zip")
+  source_code_hash = filebase64sha256("${var.lambda_source_dir}/update_prescription.zip")
 
   environment {
     variables = {
@@ -32,12 +32,12 @@ resource "aws_lambda_function" "update_prescription" {
 }
 
 resource "aws_lambda_function" "notifications" {
-  filename         = "${path.module}/notifications.zip"
+  filename         = "${var.lambda_source_dir}/notifications.zip"
   function_name    = "NotificationsFunction"
   role             = var.lambda_role_arn
   handler          = "NotificationsFunction.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("${path.module}/notifications.zip")
+  source_code_hash = filebase64sha256("${var.lambda_source_dir}/notifications.zip")
 
   environment {
     variables = {
@@ -46,5 +46,3 @@ resource "aws_lambda_function" "notifications" {
     }
   }
 }
-
-# Zip files are assumed to be created manually or via local-exec outside TF for now
